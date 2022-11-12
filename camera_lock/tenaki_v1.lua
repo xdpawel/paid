fcab45fd0f169609aec12bb481752c1aa0dfbe5b
xdpawel <94976371+xdpawel@users.noreply.github.com>
2022-11-12 16:58:13 +0100

    Create tenaki_v1.lua

---
+++ camera_lock/tenaki_v1.lua
@@ -0,0 +1,13 @@
+getgenv().RecurringPoint = "UpperTorso"
+getgenv().Hitbox = "UpperTorso"
+getgenv().Keybind = "q"
+getgenv().AimbotStrengthAmount = 0.0421
+getgenv().PredictionAmount = 11
+getgenv().Radius = 25
+getgenv().UsePrediction = true
+getgenv().AimbotStrength = true
+getgenv().FirstPerson = true
+getgenv().ThirdPerson = true
+getgenv().TeamCheck = false
+getgenv().Enabled = true
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tenaaki/GenericAimbot/main/v1.0.0"))()
